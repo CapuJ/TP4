b123d550976b1a1829873d235c980e9caa86ed39
Capucine <capucine.jumelle@cpe.fr>
2021-12-17 09:45:27 +0100

    new commit

--- interface_graphique.py
+++ interface_graphique.py
@@ -8,22 +8,22 @@ Interface du jeu Space Invaders
 
 #Importation des modules nécessaires
 
-from tkinter import Button, Label, Tk, Canvas
+from tkinter import Button, Tk, Canvas
 
 #Programme principal
 
 
 
 fenetre = Tk()
-
 fenetre.title("Space invaders")
 score = Label(fenetre, text='Score:')
 score.grid(row=0, column=0, sticky='w')
 jeu = Canvas(fenetre, bg= 'black')
-jeu.grid(row=1, column= 0, rowspan=2)
+jeu.pack(side='left')
 bouton_recommencer = Button(fenetre, text="New game")
 bouton_recommencer.grid(row=1, column=1)
 bouton_quitter = Button(fenetre, text="Quit", command=fenetre.destroy)
-bouton_quitter.grid(row=2, column=1)
-
+bouton_quitter.pack(side='top', padx=5, pady=10)
 fenetre.mainloop()
+
+
