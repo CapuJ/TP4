9c9454822e5f8f8c0a428d15c614346f398f50ec
Capucine <capucine.jumelle@cpe.fr>
2022-01-07 11:09:22 +0100

    image fond+ tir vaisseau

--- interface_graphique.py
+++ interface_graphique.py
@@ -6,7 +6,7 @@ Interface du jeu Space Invaders
 """
 
 #Importation des modules nécessaires
-from tkinter import Button, Label, Tk, Canvas, messagebox
+from tkinter import Button, Label, PhotoImage, Tk, Canvas, messagebox
 
 # Initialisation
 
@@ -21,20 +21,22 @@ pas_x = 2
 pas_y = 15
 tirs_alien  =[]
 
+
 Largeur=480
 Hauteur=320
 
+vie=1
 # Fonctions
-PosX=Largeur/2
+PosX=Largeur/2      # #POUR LE VAISSEAU
 PosY=Hauteur-15
 
-xtir=PosX-10
+xtir=PosX-10    #POUR LE TIR DU VAISSEAU
 ytir=PosY+10
 
 #Programme principal
 
 def deplacement_alien():
-    global x0_alien, y0_alien, x1_alien, y1_alien, rayon, sens, PosX, PosY
+    global x0_alien, y0_alien, x1_alien, y1_alien, rayon, sens, PosX, PosY, vie
     if (x0_alien + sens * pas_x) < 0 or (x1_alien + sens * pas_x) > Largeur:
         sens *= -1
         y0_alien = y0_alien + pas_y
@@ -44,7 +46,9 @@ def deplacement_alien():
     jeu.coords(alien, x0_alien, y0_alien, x1_alien, y1_alien)
     if (PosY - 10 < y1_alien < PosY + 10) and ((PosX - 10 < x0_alien< PosX + 10 ) or (PosX - 10 < x1_alien < PosX + 10)):
         jeu.delete(vaisseau)
-        messagebox.showinfo('', 'Vous avez perdu !')
+        vie-=1
+        if vie==0:
+            messagebox.showinfo('', 'Vous avez perdu !')
     else:
         fenetre.after(20, deplacement_alien)
 
@@ -54,7 +58,7 @@ def creer_tir_alien():
     x_tir = x0_alien + rayon
     y0_tir = y1_alien
     y1_tir = y1_alien + longueur_tir_alien
-    tir_alien = jeu.create_line(x_tir, y0_tir, x_tir, y1_tir, fill='yellow')
+    #tir_alien = jeu.create_line(x_tir, y0_tir, x_tir, y1_tir, fill='yellow')
     deplacement_tir_alien()
     
 def deplacement_tir_alien():
@@ -73,11 +77,15 @@ def deplacement_tir_alien():
 
 
 def deplacement_tir():
-    global ytir, rayon, sens, pas
+    global ytir, xtir, x1_alien, y1_alien
     ytir -= 10
     jeu.coords(tir, PosX, ytir-10, xtir+10, ytir+10)
-    fenetre.after(20, deplacement_tir)
-
+    if (y1_alien - 10 < ytir < y1_alien + 10) and ((x0_alien - 10 < PosX < x0_alien + 10 ) or (x1_alien- 10 < PosX < x1_alien + 10)):
+        jeu.delete(alien)
+        jeu.delete(tir)
+        messagebox.showinfo('Youpi!', 'Félicitations! Vous avez gagné!')
+    else:
+        fenetre.after(20, deplacement_tir)
 
 
 def Clavier(event):
@@ -106,13 +114,19 @@ def Clavier(event):
 
 fenetre = Tk()
 fenetre.title("Space invaders")
+background_image=PhotoImage("fond.png")
+background_label =Label(image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
 score = Label(fenetre, text='Score:')
 score.grid(row=0, column=0, sticky='w')
+vies = Label(fenetre, text='Vies:  /3')
+vies.grid(row=0, column=1, sticky='w')
 jeu = Canvas(fenetre, bg= 'dark blue', width=Largeur, height=Hauteur)
 jeu.grid(row=1, column= 0, rowspan=2)
-bouton_recommencer = Button(fenetre, text="New game", activebackground="cyan", background="green")
+bouton_recommencer = Button(fenetre, text="Nouveau Jeu", activebackground="cyan", background="green")
 bouton_recommencer.grid(row=1, column=1)
-bouton_quitter = Button(fenetre, text="Quit Game", activebackground="cyan", background="red", command=fenetre.destroy)
+bouton_quitter = Button(fenetre, text="Quitter le jeu", activebackground="cyan", background="red", command=fenetre.destroy)
 bouton_quitter.grid(row=2, column=1)
 
 alien = jeu.create_oval(x0_alien, y0_alien, x1_alien, y1_alien, fill="green")
@@ -120,7 +134,9 @@ deplacement_alien()
 
 fenetre.after(1000, creer_tir_alien)
 
+tir=jeu.create_rectangle(PosX, PosY-10, xtir+10, ytir+100, width=2, outline='black', fill='cyan')
 vaisseau=jeu.create_rectangle(PosX-10,PosY-10, PosX+10, PosY+10,width=5, outline='dark cyan', fill='cyan')
+
 
 
 jeu.focus_set()
@@ -130,7 +146,7 @@ jeu.bind('<Key>', Clavier)
 #tir=jeu.create_line(PosX, PosY-10, xtir+10, ytir+10, fill="yellow")
 
 
-tir=jeu.create_line(PosX, PosY-10, xtir+10, ytir+10, fill="yellow")
+#tir=jeu.create_line(PosX, PosY-10, xtir+10, ytir+10, fill="yellow")
 
 
 fenetre.mainloop() 
