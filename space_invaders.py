f3e036a985105f3aac9b15e042f32d504db40ace
Thomas <thomas.gabriel@cpe.fr>
2022-01-23 15:37:08 +0100

    update

--- space_invaders.py
+++ space_invaders.py
@@ -7,11 +7,10 @@ Interface du jeu Space Invaders
 
 ## Importation des modules nécessaires ##
 
-import tkinter
-from tkinter import Button, Label, Tk, Canvas, messagebox, PhotoImage
+
+from tkinter import Button, Label, Tk, Canvas, messagebox, PhotoImage, StringVar
 import os
 from random import randint
-from time import sleep
 
 ## Initialisation ##
 
@@ -19,6 +18,7 @@ largeur=960
 hauteur=640
 tirs_alien = []
 tirs_vaisseau = []
+score = 0
 
 
 class alien: 
@@ -40,39 +40,54 @@ class alien:
             self.x1 = self.x1 + pas_x * sens
         jeu.coords(self.id_tk, self.x0, self.y0, self.x1, self.y1)
     def tir(self, tirs_alien, jeu):
-        tirs_alien += [tir_alien(self, 15, jeu)]
+        tirs_alien += [tir_alien(self, 15, jeu, vaisseau1, vies)]
     def collision(self, vaisseau):
-        return vaisseau1.y1 < self.y1 < vaisseau.y0 and (vaisseau.x0 <self.x0 < vaisseau.x1) or (vaisseau.x0 < self.x1 < vaisseau.x1)
+        return vaisseau.y1 < self.y1 < vaisseau.y0 and (vaisseau.x0 <self.x0 < vaisseau.x1) or (vaisseau.x0 < self.x1 < vaisseau.x1)
 
 
 class tir_alien:
-    def __init__(self, alien, longueur, jeu):
+    def __init__(self, alien, longueur, jeu, cible, vies):
         self.x = alien.x0 + alien.rayon
         self.y0 = alien.y1
         self.y1 = alien.y1 + longueur
         self.id_tk = jeu.create_line(self.x, self.y0, self.x, self.y1, fill="yellow")
-        self.deplacement(jeu)
-    def deplacement(self, jeu):
+        self.deplacement(jeu, cible, vies)
+    def deplacement(self, jeu, cible, vies):
         self.y0 += 10
         self.y1 += 10
         jeu.coords(self.id_tk, self.x, self.y0, self.x, self.y1)
-        if (vaisseau1.y1 + 10 < self.y0 < vaisseau1.y1 - 10) and ((vaisseau1.x0 + 10 < self.x < vaisseau1.x0 - 10 ) or (vaisseau1.x1 + 10 < self.x < vaisseau1.x1 - 10)):
-            jeu.delete(vaisseau1.id_tk)
+        collision = self.collision(cible, vies, jeu)
+        if self.y0 > hauteur:
             jeu.delete(self.id_tk)
-            messagebox.showinfo('Youpi!', 'echec!')
-        else:
-            jeu.after(20, self.deplacement, jeu)
+            tirs_alien.remove(self)
+        elif not collision:
+            jeu.after(20, self.deplacement, jeu, cible, vies)
+    def collision(self, cible, vies, jeu):
+        meme_ligne = cible.y1 < self.y1 < cible.y0
+        meme_colonne = cible.x0 < self.x < cible.x1
+        if meme_colonne and meme_ligne:
+            jeu.delete(self.id_tk)
+            tirs_alien.remove(self)
+            vies.update()
+            if vies.int_vie == 0:
+                messagebox.showinfo('Game over', 'Vous avez perdu')
+            return True
+        return False
+
+
+
 
 class vaisseau:
     def __init__(self,jeu):
-        self.x0 = largeur/2 -10
+        self.x0 = largeur/2 - 20
         self.y0 = hauteur - 5
-        self.x1 = largeur/2 + 10
-        self.y1 = hauteur - 25
-        self.id_tk = jeu.create_rectangle(self.x0, self.y0, self.x1, self.y1, width=5, outline='dark cyan', fill='cyan')
+        self.x1 = largeur/2 + 20
+        self.y1 = hauteur - 45
+        self.charge = True
+        self.id_tk = jeu.create_rectangle(self.x0, self.y0, self.x1, self.y1, fill='cyan')
     def deplacement(self, sens, jeu):
-        self.x0 += sens * 20
-        self.x1 += sens * 20
+        self.x0 += sens * 10
+        self.x1 += sens * 10
         if self.x0 < 0:
             self.x1 = largeur
             self.x0 = largeur - 20
@@ -80,27 +95,31 @@ class vaisseau:
             self.x0 = 0
             self.x1 = 20
         jeu.coords(self.id_tk, self.x0, self.y0, self.x1, self.y1)
-    def tir(self, tirs_vaisseau, jeu, groupe):
-        tirs_vaisseau += [tir_vaisseau(self, 20, jeu, tirs_vaisseau, groupe)]
-
+    def tir(self, tirs_vaisseau, jeu, groupe, score):
+        if self.charge:
+            tirs_vaisseau += [tir_vaisseau(self, 20, jeu, tirs_vaisseau, groupe, score)]
+            self.charge = False
+            jeu.after(500, self.reload)
+    def reload(self):
+        self.charge = True
 class tir_vaisseau:
-    def __init__(self, vaisseau, longueur, jeu, tirs_vaisseau, groupe):
+    def __init__(self, vaisseau, longueur, jeu, tirs_vaisseau, groupe, score):
         self.x = vaisseau.x0 + 10
         self.y0 = vaisseau.y0 - longueur
         self.y1 = vaisseau.y0
         self.id_tk = jeu.create_line(self.x, self.y0, self.x, self.y1, fill="red")
-        self.deplacement(jeu, tirs_vaisseau, groupe)
-    def deplacement(self, jeu, tirs_vaisseau, groupe):
+        self.deplacement(jeu, tirs_vaisseau, groupe, score)
+    def deplacement(self, jeu, tirs_vaisseau, groupe, score):
         self.y0 -= 10
         self.y1 -= 10
         jeu.coords(self.id_tk, self.x, self.y0, self.x, self.y1)
-        collision = self.collision(groupe, tirs_vaisseau, jeu)
+        collision = self.collision(groupe, tirs_vaisseau, jeu, score)
         if self.y0 < 0:
             jeu.delete(self.id_tk)
             tirs_vaisseau.remove(self)
         elif not collision:
-            jeu.after(20, self.deplacement, jeu, tirs_vaisseau, groupe)
-    def collision(self, groupe, tirs_vaisseau, jeu):
+            jeu.after(20, self.deplacement, jeu, tirs_vaisseau, groupe, score)
+    def collision(self, groupe, tirs_vaisseau, jeu, score):
         for alien in groupe.aliens:
             meme_ligne = alien.y0 < self.y0 < alien.y1
             meme_colonne = alien.x0 < self.x < alien.x1
@@ -109,8 +128,10 @@ class tir_vaisseau:
                 tirs_vaisseau.remove(self)
                 jeu.delete(alien.id_tk)
                 groupe.aliens.remove(alien)
+                score.update()
                 return True
         return False
+        
 class groupe_aliens:
     def __init__(self, jeu, nb_lignes, nb_colonnes, rayon_alien):
         self.aliens = []
@@ -154,16 +175,27 @@ class groupe_aliens:
     def tir(self, tirs_alien, jeu):
         n = len(self.aliens)
         ind_tireur = randint(0, n - 1)
-        print(ind_tireur)
         tireur = self.aliens[ind_tireur]
         tireur.tir(tirs_alien, jeu)
-        delai = randint(2000, 5000)
+        delai = randint(500, 1500)
         jeu.after(delai, self.tir, tirs_alien, jeu)
 
-
-    
-            
-
+class vie:
+    def __init__(self):
+        self.int_vie = 3
+        self.str_vie = StringVar()
+        self.str_vie.set('Vie:  ' + str(self.int_vie) + '/3')
+    def update(self):
+        self.int_vie -= 1
+        self.str_vie.set('Vie:  ' + str(self.int_vie) + '/3')
+class score:
+    def __init__(self):
+        self.int_score = 0
+        self.str_score = StringVar()
+        self.str_score.set('Score:  ' + str(self.int_score))
+    def update(self):
+        self.int_score += 10
+        self.str_score.set('Score:  ' + str(self.int_score))
 
 
 def Clavier(event):
@@ -173,7 +205,7 @@ def Clavier(event):
     if touche =='Left':
         vaisseau1.deplacement(-1, jeu)
     if touche =='space':
-        vaisseau1.tir(tirs_vaisseau, jeu, groupe)
+        vaisseau1.tir(tirs_vaisseau, jeu, groupe, score1)
         
 
 ###def Forme() #il prenne la forme de base du vaisseau et mette une photo dessus
@@ -197,18 +229,20 @@ chemin = os.path.join(os.path.dirname(__file__), "f2.gif") #permet de trouver a 
 photo=PhotoImage(file=chemin)
 
 #affichage du score
-score = Label(fenetre, text='Score:')
-score.grid(row=0, column=0, sticky='w')
+score1 = score()
+score_label = Label(fenetre, textvariable=score1.str_score)
+score_label.grid(row=0, column=0, sticky='w')
 
 #affichage du nombre de vies
-vies = Label(fenetre, text='Vies:  /3')
-vies.grid(row=0, column=1, sticky='w')
+vies = vie()
+vies_label = Label(fenetre, textvariable=vies.str_vie)
+vies_label.grid(row=0, column=1, sticky='w')
 
 #couleur de fond 
 jeu = Canvas(fenetre, bg= 'dark blue', width=largeur, height=hauteur)
 
 #création de l'image de fond sur le canvas
-item= jeu.create_image(0,0, image=photo, anchor = tkinter.NW)
+item= jeu.create_image(0,0, image=photo, anchor = 'nw')
 
 #création de la grille
 jeu.grid(row=1, column= 0, rowspan=2)
@@ -224,6 +258,7 @@ bouton_quitter.grid(row=2, column=1)
 vaisseau1 = vaisseau(jeu)
 delai = randint(2000, 5000)
 groupe = groupe_aliens(jeu, 9, 4, 25)
+
 
 
 
